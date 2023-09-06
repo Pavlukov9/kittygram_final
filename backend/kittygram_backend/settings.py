6b4baea64a54fa209e95d830b1f66f7e845deee2
Даниил Павлюков <pavlukov.daniil@yandex.ru>
2023-09-06 22:28:45 +0300

    Исправил замечания

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,6 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = True
+# На счёт переменных окружения: сделал как обычно через .env,
+# но GitHub Actions не пропускает это и выдаёт ошибку,
+# что не может найти SECRET_KEY. Попробовал даже создать secret в
+# в настройках репозитория и всё равно исход тот же - ошибка(
 
 ALLOWED_HOSTS = []
 
